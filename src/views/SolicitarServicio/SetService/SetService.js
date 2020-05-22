28111a3e6b4d58bc6363229f3b69e1736f7d9628
Michael Sosa <833@holbertonschool.com>
2020-05-22 09:05:44 -0500

    Displaying services in cards

--- src/views/SolicitarServicio/SetService/SetService.js
+++ src/views/SolicitarServicio/SetService/SetService.js
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
 import { getServices } from "../../../controllers/apiRequests";
 import { AuthContext } from "../../../contexts/AuthContext";
-import { Form, Col, Table, Badge } from "react-bootstrap";
-import { AiFillDollarCircle } from "react-icons/ai";
-
+import { Container } from "react-bootstrap";
+// import { AiFillDollarCircle } from "react-icons/ai";
+import Service from "./Service/Service";
 import "./SetService.scss";
 
 const SetService = (props) => {
@@ -18,66 +18,25 @@ const SetService = (props) => {
     fetcServices();
   }, []);
 
-  // ===============================CHECK CREDITS AND SET SERVICE ==================================================
-
-  const handleClick = (e, service) => {
-    const companyCreds = userInfoContext.company.credit;
-    const serviceCreds = service.ride_value;
-    if (companyCreds >= serviceCreds) {
-      e.target.parentNode.parentNode.childNodes.forEach((e) =>
-        e.classList.remove("active")
-      );
-      let row = document.getElementById(service.id);
-      row.classList.add("active");
-      props.setService(service);
-    } else {
-      alert("Creditos insuficientes");
-    }
+  // =============================== SET SERVICE ==================================================
+  const handleClick = (service) => {
+    props.setService(service);
   };
+
   return (
-    <Form className="setService">
-      <Form.Row>
-        <Form.Group as={Col}>
-          <Table responsive size="sm" className="text-center">
-            <thead>
-              <tr className="test">
-                <th>Servicio</th>
-                <th>Tipo</th>
-                <th>Descripcion</th>
-                <th>Requerimientos</th>
-                <th>Duracion</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {services.map((service) => {
-                return (
-                  <tr
-                    onClick={(e) => handleClick(e, service)}
-                    key={service.id}
-                    id={service.id}
-                  >
-                    <td>{service.name}</td>
-                    <td>{service.service_type}</td>
-                    <td>{service.description}</td>
-                    <td>{service.requirements}</td>
-                    <td>{service.duration} min</td>
-                    <td>
-                      {/* {service.ride_value} */}
-                      <Badge>
-                        <AiFillDollarCircle />
-                        <small>{service.ride_value}</small>
-                      </Badge>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-          {/* {errors.level && <small>Por favor seleccione una opcion</small>} */}
-        </Form.Group>
-      </Form.Row>
-    </Form>
+    <Container className="mt-5">
+      <div class="card-deck mb-3 text-center">
+        {services.map((service) => {
+          return (
+            <Service
+              service={service}
+              handleClick={handleClick}
+              userInfoContext={userInfoContext}
+            />
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
