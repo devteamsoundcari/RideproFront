616eb52c463d3285841b36444247e6c8e203179f
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-06-28 00:19:06 -0500

    feat: interactivity for company logo edit modal
    Implemented visual stages indicating image uploading through the API and
    a success message indicating the completion of the image upload.

--- src/views/Company/CompanyLogoEditModal.tsx
+++ src/views/Company/CompanyLogoEditModal.tsx
@@ -8,27 +8,28 @@ import {
   Form,
   Button,
   Image,
+  Spinner,
 } from "react-bootstrap";
-import { setCompanyLogo, getUserInfo } from "../../controllers/apiRequests";
+import { FaCheckCircle } from "react-icons/fa";
+import { setCompanyLogo } from "../../controllers/apiRequests";
 import "./CompanyLogoEditModal.scss";
 
-
 const CompanyLogoEditModal = (props: any) => {
-  const {
-    userInfoContext,
-    setUserInfoContext,
-    updateUserInfo
-  } = useContext(AuthContext);
+  const { userInfoContext, updateUserInfo } = useContext(AuthContext);
   const { company } = userInfoContext;
+  const [stage, setStage] = useState("waiting");
   const [imageName, setImageName] = useState("Importar imagen");
   const [imgSrc, setImgSrc] = useState<string | ArrayBuffer | null>();
   const [selectedImage, setSelectedImage] = useState<HTMLImageElement>();
 
   const save = async (e: any) => {
+    setStage("loading");
     e.preventDefault();
     setCompanyLogo(company.id, selectedImage).then(async (result) => {
       if (result.status === 200) {
-        updateUserInfo();
+        updateUserInfo().then(() => {
+          setStage("success");
+        });
       }
     });
   };
@@ -50,41 +51,84 @@ const CompanyLogoEditModal = (props: any) => {
     };
   };
 
+  const handleExit = () => {
+    props.onHide();
+  }
 
-  return (
-    <Modal show={props.show} onHide={props.onHide} size="sm">
-      <Modal.Header closeButton>
-        <Modal.Title>Logo</Modal.Title>
-      </Modal.Header>
-      <Form className="upload-company-logo-form" onSubmit={save}>
+  const reset = () => {
+    setStage("waiting");
+  }
+
+  const loadingMessage = () => {
+    return (
+      <>
         <Modal.Body>
-          <Container fluid>
-            <Row>
-              <Col>
-                <Image src={imgSrc ? imgSrc : company.logo} fluid />
-              </Col>
-            </Row>
-            <Row>
-              <Form.Group as={Col} className="upload-file">
-              <Form.File
-               name="logo"
-               onChange={onFileUpload}
-               className="custom-file-label"
-               label={imageName}
-               custom
-              />
-              </Form.Group>
-            </Row>
-          </Container>
+          <Spinner animation="border" variant="danger" />
+        </Modal.Body>
+      </>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <>
+        <Modal.Body>
+          <h2>
+            La imagen ha sido cambiada exitosamente. <FaCheckCircle />
+          </h2>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" size="sm" type="submit">
-            Guardar
+          <Button variant="secondary" onClick={handleExit}>
+            Regresar
           </Button>
         </Modal.Footer>
-      </Form>
-    </Modal>
-  );
+      </>
+    );
+  };
+
+  const uploadFileForm = () => {
+    return (
+      <>
+        <Modal.Header closeButton>
+          <Modal.Title>Logo</Modal.Title>
+        </Modal.Header>
+        <Form className="upload-company-logo-form" onSubmit={save}>
+          <Modal.Body>
+            <Container fluid>
+              <Row>
+                <Col>
+                  <Image src={imgSrc ? imgSrc : company.logo} fluid />
+                </Col>
+              </Row>
+              <Row>
+                <Form.Group as={Col} className="upload-file">
+                  <Form.File
+                    name="logo"
+                    onChange={onFileUpload}
+                    className="custom-file-label"
+                    label={imageName}
+                    custom
+                  />
+                </Form.Group>
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" size="sm" type="submit">
+              Guardar
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </>
+    );
+  };
+
+  return (
+    <Modal show={props.show} onHide={props.onHide} onExited={reset} size="sm" centered>
+      {stage === "loading" && loadingMessage()}
+      {stage === "success" && successMessage()}
+      {stage === "waiting" && uploadFileForm()}
+    </Modal>);
 };
 
 export default CompanyLogoEditModal;
