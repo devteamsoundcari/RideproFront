64beaea7b5032cfe9bb5a64441eebbf30224fbfc
Michael <michael.sosa@holbertonschool.com>
2020-06-30 17:36:15 -0500

    Update favicon and newpassword redirection'

--- src/views/NavBar/NavBar.js
+++ src/views/NavBar/NavBar.js
@@ -61,19 +61,19 @@ const NavBar = () => {
       }
     });
   });
-  
+
   const hideProfileEditModal = () => {
     setShowProfileEditModal(false);
-  }
+  };
 
   const profileEditModal = () => {
     return (
       <ProfileEditModal
-       show={showProfileEditModal}
-       onHide={hideProfileEditModal} 
-      /> 
-    ) 
-  }
+        show={showProfileEditModal}
+        onHide={hideProfileEditModal}
+      />
+    );
+  };
 
   return (
     <>
@@ -86,14 +86,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#link">
+            <Nav.Link>
               <FaBell />
             </Nav.Link>
             <div className="userOptions">
               <NavDropdown
                 alignRight
                 title={`${userInfoContext.name} ${userInfoContext.lastName}`}
-                id="basic-nav-dropdown" >
+                id="basic-nav-dropdown"
+              >
                 <NavDropdown.Item onClick={() => setShowProfileEditModal(true)}>
                   <FaUser /> Perfil
                 </NavDropdown.Item>
