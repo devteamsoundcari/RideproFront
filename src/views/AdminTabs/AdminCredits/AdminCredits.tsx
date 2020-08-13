31041fdf2ce0f1ce9e3436ec77b59ec981bcf1e8
Michael <michael.sosa@holbertonschool.com>
2020-08-13 12:52:43 -0500

    Working on admin features

--- src/views/AdminTabs/AdminCredits/AdminCredits.tsx
+++ src/views/AdminTabs/AdminCredits/AdminCredits.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AllUsers from "../Usuarios/AllUsers/AllUsers";
+import { getUsers } from "../../../controllers/apiRequests";
+import { Button } from "react-bootstrap";
+import NewCredit from "./NewCredit";
 
 type AdminCreditsProps = any;
 const AdminCredits: React.FC<AdminCreditsProps> = () => {
-  return <div>Agregar o quitar creditos a los usuarios</div>;
+  const [users, setUsers] = useState<any>([]);
+  const [showNewCredit, setShowNewCredit] = useState(true);
+
+  const fetchUsers = async (url) => {
+    const response = await getUsers(url);
+    setUsers((oldArr) => [...oldArr, ...response.results]);
+    if (response.next) {
+      return await fetchUsers(response.next);
+    }
+  };
+  useEffect(() => {
+    fetchUsers(`${process.env.REACT_APP_API_URL}/api/v1/users/`);
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <React.Fragment>
+      <Button variant="primary" onClick={() => setShowNewCredit(true)}>
+        Asignar créditos
+      </Button>
+      {showNewCredit && (
+        <NewCredit handleClose={() => setShowNewCredit(false)} users={users} />
+      )}
+      <AllUsers users={users} />
+    </React.Fragment>
+  );
 };
 export default AdminCredits;
---
+++ src/views/AdminTabs/Companies/AllCompanies.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { Card, Image } from "react-bootstrap";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import ToolkitProvider from "react-bootstrap-table2-toolkit";
+import { getCompanies } from "../../../controllers/apiRequests";
+type AllCompaniesProps = any;
+
+const AllCompanies: React.FC<AllCompaniesProps> = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    async function fetchCompanies() {
+      let res = await getCompanies();
+      if (res) {
+        setCompanies(res.results);
+      } else {
+        console.log("No hay empresas registradas");
+      }
+    }
+    fetchCompanies();
+  }, []);
+
+  const logoFormatter = (cell) => {
+    return <Image src={cell} roundedCircle width="50" height="50" />;
+  };
+
+  const columns = [
+    {
+      dataField: "logo",
+      text: "Logo",
+      formatter: logoFormatter,
+      classes: "md-column",
+      headerClasses: "md-column",
+    },
+    {
+      dataField: "name",
+      text: "Nombre",
+      sort: true,
+    },
+
+    {
+      dataField: "nit",
+      text: "Nit",
+      sort: true,
+    },
+    {
+      dataField: "address",
+      text: "Dirección",
+    },
+    {
+      dataField: "phone",
+      text: "Teléfono",
+    },
+    {
+      dataField: "arl",
+      text: "ARL",
+    },
+  ];
+
+  const MySearch = (props) => {
+    let input;
+    const handleChange = () => {
+      props.onSearch(input.value);
+    };
+    return (
+      <div>
+        <input
+          className="form-control"
+          // style={{ backgroundColor: "pink" }}
+          placeholder="Buscar..."
+          ref={(n) => (input = n)}
+          onChange={handleChange}
+          type="text"
+        />
+      </div>
+    );
+  };
+  return (
+    <Card className="allUsers mt-3 mb-5">
+      <Card.Body>
+        <Card.Title>Empresas</Card.Title>
+        <Card.Body>
+          <ToolkitProvider
+            bootstrap4
+            // defaultSorted={defaultSorted}
+            keyField="id"
+            data={companies}
+            columns={columns}
+            pagination={paginationFactory()}
+            hover
+            search
+          >
+            {(props) => (
+              <div>
+                <MySearch {...props.searchProps} />
+                <hr />
+                <BootstrapTable {...props.baseProps} />
+              </div>
+            )}
+          </ToolkitProvider>
+        </Card.Body>
+      </Card.Body>
+    </Card>
+  );
+};
+export default AllCompanies;
