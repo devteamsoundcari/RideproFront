5b72cf149e53f522ab3c1bf9b118d86468e22a63
mikesosa <michael.sosa@coxautoinc.com>
2022-01-10 08:08:38 -0500

    login redesing'

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,15 +6,7 @@ import { Link } from 'react-router-dom';
 import './Login.scss';
 import bgImage from '../../assets/img/loginImage.png';
 import logo from '../../assets/img/logo.png';
-import {
-  Container,
-  Col,
-  Card,
-  Row,
-  Button,
-  Form,
-  Spinner
-} from 'react-bootstrap';
+import { Button, Form, Spinner } from 'react-bootstrap';
 
 export const Login = () => {
   let navigate = useNavigate();
@@ -29,103 +21,69 @@ export const Login = () => {
     }
   };
 
-  // ====================== RETURN ======================
-
   return (
-    <>
-      <Container className="justify-content-md-center d-flex mt-5 mb-5 loginForm">
-        <Col md={8}>
-          <Card className="mb-0">
-            <Row className="m-0">
-              <Col md={6} sm={12}>
-                <Card.Body>
-                  <Card.Title className="text-center">
-                    <img src={logo} alt="RideproLogo" />
-                    <h1 className="h3 mb-2 mt-1 font-weight-normal">
-                      Iniciar sesión
-                    </h1>
-                  </Card.Title>
-                  <Card.Text className="text-center">
-                    Gracias por registrarte en RIDE PRO. Tu cuenta y perfil de
-                    usuario será validada por nuestro equipo.{' '}
-                  </Card.Text>
-                  <Card.Body>
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                      <Form.Group>
-                        <Form.Label>Tu email</Form.Label>
-                        <Form.Control
-                          name="email"
-                          type="email"
-                          placeholder="Tu email"
-                          autoComplete="off"
-                          ref={register({
-                            required: true,
-                            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-                          })}
-                        />
-                        <Form.Text className="text-muted">
-                          {errors.email && (
-                            <span>Por favor, ingresa un email válido</span>
-                          )}
-                        </Form.Text>
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Tu contraseña</Form.Label>
-                        <Form.Control
-                          name="password"
-                          type="password"
-                          placeholder="Tu contraseña"
-                          ref={register({ required: true })}
-                          autoComplete="off"
-                        />
-                        <Form.Text className="text-muted">
-                          {errors.password && <span>Contraseña inválida</span>}
-                        </Form.Text>
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Check type="checkbox" label="Recuérdame" />
-                      </Form.Group>
-                      <Button
-                        variant="primary"
-                        type="submit"
-                        className="m-auto">
-                        Ingresar
-                        {loadingAuth && (
-                          <Spinner animation="border" size="sm" />
-                        )}
-                      </Button>
-                      <Link to="/reset-password">
-                        <small>Olvidé mi contraseña</small>
-                      </Link>
-                      {authError && (
-                        <p className="text-danger">{authError.message}</p>
-                      )}
-                    </Form>
-                  </Card.Body>
-                </Card.Body>
-              </Col>
-              <Col
-                md={6}
-                className="bgImageLogin"
-                style={{
-                  background: `url(${bgImage}) no-repeat center center`
-                }}></Col>
-            </Row>
-          </Card>
-        </Col>
-      </Container>
-      <footer className="sticky-footer">
-        <div className="container my-auto">
-          <div className="copyright text-center my-auto">
-            <span>
-              Copyright © Ridepro 2021 - Desarrollado por{' '}
-              <a href="https://soundlutions.com/en/">
-                <span className="text-danger"> soundlutions.com</span>
-              </a>
-            </span>
-          </div>
-        </div>
-      </footer>
-    </>
+    <div
+      className="login-page"
+      style={{
+        background: `url(${bgImage}) no-repeat center center`
+      }}>
+      <div className="form-container mb-5 text-center">
+        <img src={logo} alt="RideproLogo" />
+        <Form onSubmit={handleSubmit(onSubmit)} className="text-center">
+          <Form.Group className="mb-0">
+            <Form.Label className="text-white">Tu email</Form.Label>
+            <Form.Control
+              className="rounded-pill p-4"
+              name="email"
+              type="email"
+              placeholder="Tu email"
+              autoComplete="off"
+              ref={register({
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+              })}
+            />
+            <Form.Text className="text-muted">
+              {errors.email && <span>Por favor, ingresa un email válido</span>}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="text-white">Tu contraseña</Form.Label>
+            <Form.Control
+              className="rounded-pill p-4"
+              name="password"
+              type="password"
+              placeholder="Tu contraseña"
+              ref={register({ required: true })}
+              autoComplete="off"
+            />
+            <Form.Text className="text-muted">
+              {errors.password && <span>Contraseña inválida</span>}
+            </Form.Text>
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-100 rounded-pill p-3 mb-2 mt-3 font-weight-bold">
+            {loadingAuth ? <Spinner animation="border" size="sm" /> : 'Ingresar'}
+          </Button>
+          <Link to="/reset-password" className="text-white">
+            <Form.Label className="text-white"> Olvidé mi contraseña</Form.Label>
+          </Link>
+
+          {authError && <p className="text-danger">{authError.message}</p>}
+        </Form>
+      </div>
+
+      <div className="text-white mt-5">
+        <small>
+          Copyright ©Ridepro2022 - Desarrollado por{' '}
+          <a href="https://soundlutions.com/en/">
+            <strong className="text-white"> soundlutions.com</strong>
+          </a>
+        </small>
+      </div>
+    </div>
   );
 };
--- src/pages/PasswordRecover/PasswordRecover.tsx
+++ src/pages/PasswordRecover/PasswordRecover.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { ModalSuccess } from '../../components/molecules';
-import {
-  Container,
-  Col,
-  Card,
-  Row,
-  Button,
-  Form,
-  Spinner
-} from 'react-bootstrap';
+import { Button, Form, Spinner } from 'react-bootstrap';
+import bgImage from '../../assets/img/loginImage.png';
 import logo from '../../assets/img/logo.png';
 import { passwordReset } from '../../controllers/apiRequests';
 import './PasswordRecover.scss';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const PasswordRecover = (props) => {
+  let navigate = useNavigate();
   const [email, setEmail] = useState(null);
   const [smShow, setSmShow] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -34,61 +29,53 @@ export const PasswordRecover = (props) => {
 
   const handleClose = () => {
     setSmShow(false);
-    props.comeBack();
+    navigate('/');
   };
 
   return (
-    <Container className="password-recover">
-      <Row className="justify-content-md-center">
-        <Col md={6} sm={12}>
-          <Card>
-            <Card.Body>
-              <Card.Title className="text-center">
-                <img className="mb-4 company-logo" src={logo} alt="" />
-                <h1 className="h3 mb-3 font-weight-normal">
-                  Recuperar contraseña
-                </h1>
-              </Card.Title>
-              <Card.Text className="text-center">
-                Ingresa tu email. Recibirás un correo con instrucciones para
-                restrablecer tu contraseña.
-              </Card.Text>
-              <Card.Body>
-                <Form onSubmit={handleSubmit(onSubmit)} className="text-center">
-                  <Form.Group>
-                    <Form.Control
-                      name="email"
-                      type="email"
-                      placeholder="Tu email"
-                      autoComplete="off"
-                      ref={register({
-                        required: true,
-                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-                      })}
-                    />
-                    <Form.Text className="text-muted">
-                      {errors.email && (
-                        <span>Por favor ingresa un email válido</span>
-                      )}
-                    </Form.Text>
-                  </Form.Group>
-                  <Link to="/login">
-                    <small>Iniciar sesion</small>
-                  </Link>
+    <div
+      className="login-page"
+      style={{
+        background: `url(${bgImage}) no-repeat center center`
+      }}>
+      <div className="form-container mb-5 text-center">
+        <img src={logo} alt="RideproLogo" />
+        <Form onSubmit={handleSubmit(onSubmit)} className="text-center">
+          <Form.Group className="mb-0">
+            <Form.Label className="text-white">Tu email</Form.Label>
+            <Form.Control
+              className="rounded-pill p-4"
+              name="email"
+              type="email"
+              placeholder="Tu email"
+              autoComplete="off"
+              ref={register({
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+              })}
+            />
+            <Form.Text className="text-white">
+              {errors.email && <span>Por favor ingresa un email válido</span>}
+            </Form.Text>
+          </Form.Group>
 
-                  <Button variant="primary" type="submit" className="m-auto">
-                    Restrablecer contraseña
-                  </Button>
-                  {loading && (
-                    <Spinner animation="border" role="status"></Spinner>
-                  )}
-                </Form>
-              </Card.Body>
-            </Card.Body>
-          </Card>
-          <ModalSuccess show={smShow} data={email} onHide={handleClose} />
-        </Col>
-      </Row>
-    </Container>
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-100 rounded-pill p-3 mb-2 mt-3 font-weight-bold">
+            Restrablecer contraseña
+          </Button>
+
+          {loading ? (
+            <Spinner animation="border" role="status" className="text-white"></Spinner>
+          ) : (
+            <Link to="/login" className="text-white">
+              <Form.Label className="text-white">Iniciar sesión</Form.Label>
+            </Link>
+          )}
+        </Form>
+        <ModalSuccess show={smShow} data={email} onHide={handleClose} />;
+      </div>
+    </div>
   );
 };
